ad00214e3ecf36c58e8a20e26bed6505a66780b0
Yong SangYoon <64957267+ryong9rrr@users.noreply.github.com>
2021-05-25 11:02:12 +0900

    #4.14 file upload 1

--- schema.js
+++ schema.js
@@ -8,8 +8,8 @@ import {
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
